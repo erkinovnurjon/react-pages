ba12733b0c2718a702a6222d5eeba6ebc3b289ce
Nurjon <nurjon2477@gmail.com>
2023-07-14 21:45:23 +0500

    Bismillah

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Main from "./containers/Main";
+import Navbar from "./containers/Navbar";
+import Sidebar from "./containers/Sidebar";
+import './pages/app.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar/>
+      <div className="content">
+      <Navbar/>
+      <Main/>
+      </div>
+    
     </div>
   );
 }
