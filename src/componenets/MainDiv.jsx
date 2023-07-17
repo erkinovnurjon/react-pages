98f4cc50c5b9340dff54dd044b6508efb9f80fc2
Nurjon <nurjon2477@gmail.com>
2023-07-17 22:48:46 +0500

    end yest oshibki

--- src/componenets/MainDiv.jsx
+++ src/componenets/MainDiv.jsx
@@ -1,7 +1,28 @@
-const MainDiv = ({image}) => {
+import "../containers/main.scss"
+import Personone from "../assets/images/icons/m 5.png";
+import "../containers/main.scss"
+const MainDiv = ({image,contact,contactp,name,nameon,date,dateon,button}) => {
       return (
             <div className="user">
+                  <div className="user__left">
+
+                  <img src={Personone} alt="person" />
+                  <span className="headingg"> <h3>{contact}</h3>
+                  <p className="gray">{contactp}</p>
+                  </span>
+                  </div>
+                  <div className="user__right">
+
+                  <div className="r_leftt">
+                  <span><h3 className="headingg">{name}</h3> <p className="gray">{nameon}</p></span>
+                  <span><h3 className="headingg">{date}</h3>  <p className="gray">{dateon}</p></span>
+                  </div>
+
+                 <div className="r__rightt">
+                 <button className="bnt btn-success">{button}</button>
                   <span>{image}</span>
+                 </div>
+                  </div>
 
 
             </div>
--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -1,6 +1,7 @@
 import { Mainone, NAvfilter, Navsort } from "../assets/icon";
 import MainDiv from "../componenets/MainDiv";
-import "./main.scss"
+import "./main.scss";
+
 const Main = () => {
       return (
             <div className="container">
@@ -28,7 +29,17 @@ const Main = () => {
                              <span>Priority</span>
                           </div>
                         </div>
-                        <MainDiv image={<Mainone/>}/>
+                        <MainDiv  image={<Mainone/>} contact="Contact Email not Linked" contactp="Updated 1 day ago" name="Tom Cruise" nameon="on 24.05.2019"  date="May 26, 2019" dateon="6:30 PM" button="High"/>
+
+                        <MainDiv  image={<Mainone/>} contact="Adding Images to Featured Posts" contactp="Updated 1 day ago" name="Matt Damon" nameon="on 24.05.2019"  date="May 26, 2019" dateon="6:00 AM" button="Low"/>
+                        <MainDiv  image={<Mainone/>} contact="When will I be charged this month?" contactp="Updated 1 day ago" name="Robert Downey" nameon="oon 24.05.2019"  date="May 26, 2019" dateon="7:30 PM" button="High"/>
+                        <MainDiv  image={<Mainone/>} contact="Payment not going through" contactp="Updated 2 days ago" name="Christian Bale" nameon="on 24.05.2019"  date="May 25, 2019" dateon="4:00 PM" button="High"/>
+                        <MainDiv  image={<Mainone/>} contact="Unable to add replies" contactp="Updated 1 day ago" name="Unable to add replies" nameon="on 24.05.2019"  date="May 26, 2019" dateon="6:00 AM" button="High"/>
+                        <MainDiv  image={<Mainone/>} contact="Downtime since last week" contactp="Updated 3 day ago" name="Chris Evans" nameon="on 23.05.2019"  date="May 26, 2019" dateon="2:00 PM" button="Normal"/>
+                        <MainDiv  image={<Mainone/>} contact="Referral Bonus" contactp="Updated 4 day ago" name="Sam Smith" nameon="on 24.05.2019"  date="May 26, 2019" dateon="11:30 AM" button="Low"/>
+                        <MainDiv  image={<Mainone/>} contact="How do I change my password?" contactp="Updated 6 day ago" name="Steve Rogers" nameon="on 24.05.2019"  date="May 24, 2019" dateon="1:00 PM" button="Normal"/>
+                        
+
 
                   </div>
 
